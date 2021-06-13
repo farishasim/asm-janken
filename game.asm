bb109d78c4413be15c4264906752372568af1bea
Faris Hasim <farishasimsyauqi@gmail.com>
2021-06-13 19:03:59 +0700

    update print & read

--- game.asm
+++ game.asm
@@ -21,14 +21,70 @@ section .data
     exitMsg db '9. exit', 0xa
     lenExitMsg equ $ - exitMsg
 
+    newline db 0xa
+
+section .bss
+    p_choice resb 1 ; player choice
+    c_choice resb 1 ; computer choice
+
+    p_count resw 1  ; player win counter
+    c_count resw 1  ; computer win counter
+
 section .text
     global _start
 
 _start:
+    call    printMenu
 
-
+    mov     ecx, p_choice
+    mov     edx, 1
+    call    printMsg
+    call    newLine
 
 exit:
-    mov eax, SYS_EXIT
-    mov ebx, 0
-    int 0x80
+    mov     eax, SYS_EXIT
+    mov     ebx, 0
+    int     0x80
+
+printMsg:
+    mov     eax, SYS_WRITE
+    mov     ebx, STDOUT
+    int     0x80
+    ret
+
+newLine:
+    mov     ecx, newline
+    mov     edx, 1
+    call    printMsg
+    ret
+
+readInput:
+    mov     eax, SYS_READ
+    mov     ebx, STDIN
+    int     0x80
+    ret
+
+printMenu:
+    mov     ecx, kertas
+    mov     edx, lenKertas
+    call    printMsg
+
+    mov     ecx, gunting
+    mov     edx, lenGunting
+    call    printMsg
+
+    mov     ecx, batu
+    mov     edx, lenBatu
+    call    printMsg
+    
+    call    newLine
+
+    mov     ecx, inputMsg
+    mov     edx, lenInputMsg
+    call    printMsg
+
+    mov     ecx, p_choice
+    mov     edx, 1
+    call    readInput
+
+    ret
