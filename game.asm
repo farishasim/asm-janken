97a70e1ee20a56d06ecca7e5b21a212dc7d02fb7
Faris Hasim <farishasimsyauqi@gmail.com>
2021-06-13 19:06:01 +0700

    update little

--- game.asm
+++ game.asm
@@ -1,34 +1,34 @@
-SYS_EXIT equ 1
-SYS_READ equ 3
-SYS_WRITE equ 4
+SYS_EXIT    equ 1
+SYS_READ    equ 3
+SYS_WRITE   equ 4
 
-STDIN equ 0
-STDOUT equ 1
+STDIN       equ 0
+STDOUT      equ 1
 
 section .data
-    inputMsg db 'Masukkan nomor pilihan : '
+    inputMsg    db 'Masukkan nomor pilihan : '
     lenInputMsg equ $ - inputMsg
 
-    kertas db '1. kertas', 0xa
-    lenKertas equ $ - kertas
+    kertas      db '1. kertas', 0xa
+    lenKertas   equ $ - kertas
     
-    gunting db '2. gunting', 0xa
-    lenGunting equ $ - gunting
+    gunting     db '2. gunting', 0xa
+    lenGunting  equ $ - gunting
 
-    batu db '3. batu', 0xa
-    lenBatu equ $ - batu
+    batu        db '3. batu', 0xa
+    lenBatu     equ $ - batu
 
-    exitMsg db '9. exit', 0xa
-    lenExitMsg equ $ - exitMsg
+    exitMsg     db '9. exit', 0xa
+    lenExitMsg  equ $ - exitMsg
 
-    newline db 0xa
+    newline     db 0xa
 
 section .bss
-    p_choice resb 1 ; player choice
-    c_choice resb 1 ; computer choice
+    p_choice    resb 1  ; player choice
+    c_choice    resb 1  ; computer choice
 
-    p_count resw 1  ; player win counter
-    c_count resw 1  ; computer win counter
+    p_count     resw 1  ; player win counter
+    c_count     resw 1  ; computer win counter
 
 section .text
     global _start
